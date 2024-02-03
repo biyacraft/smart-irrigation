18de00b85dc607be279b0f1547f2a9a8e0629cb3
tesfaa abi <abitesfahunn@gmail.com>
2024-02-04 02:24:18 +0300

    Delete hardware directory

--- hardware/src/app/routes/NotificationRoute.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const NotificationController = require("../controllers/NotificationController");
-router.get("/new", NotificationController.getUnreadNotifications);
-module.exports = router;
