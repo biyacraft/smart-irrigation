a7c16230cae82815c05dbae0202a114ccab641aa
tesfaa abi <abitesfahunn@gmail.com>
2024-02-07 22:22:15 +0300

    Update AppNavigator.js

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import RegistrationScreen from "../screens/RegistrationScreen";
 import LoginScreen from "../screens/LoginScreen";
 
+
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
